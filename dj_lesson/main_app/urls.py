d791dd1f6127c5120b84028a1d9918129886fa20
janisfs <janisfs@mail.ru>
2024-11-16 19:14:58 +0300

    add new code

--- dj_lesson/main_app/urls.py
+++ dj_lesson/main_app/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index),
-    path('new_page', views.new_page),
+    path('', views.index, name='home'),
+    path('new_page', views.new_page, name='new_page'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
